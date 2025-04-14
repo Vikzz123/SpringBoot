44da9fe9bd543ccacf935fd8a3bef4143c5405b9
vikas <vikaszenithian@gmail.com>
2025-04-14 12:50:28 +0530

    git init -m "First Commit"

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class DemoApplicationTests {
+
+	public static void  main (String[] args){
+		  SpringBootApplication.run(DemoApplicationTests.class, args);
+	}
+
+}
